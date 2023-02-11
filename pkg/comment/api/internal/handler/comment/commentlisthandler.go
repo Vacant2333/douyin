464232d765d96e9dcb15c0ce5e112c95bb6be446
db <1366022396@qq.com>
2023-02-11 10:10:35 +0800

    add comment api

---
+++ pkg/comment/api/internal/handler/comment/commentlisthandler.go
@@ -0,0 +1,28 @@
+package comment
+
+import (
+	"net/http"
+
+	"comment/api/internal/logic/comment"
+	"comment/api/internal/svc"
+	"comment/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func CommentListHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.CommentListReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := comment.NewCommentListLogic(r.Context(), svcCtx)
+		resp, err := l.CommentList(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
