8d2fcc97e2916eeb48ce9c0cd4989bd1c26ed35b
db <1366022396@qq.com>
2023-02-18 14:14:33 +0800

    add comment mq

---
+++ pkg/comment/mq/internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/zeromicro/go-queue/kq"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+type Config struct {
+	service.ServiceConf
+
+	// redis
+	RedisCacheConf redis.RedisConf
+
+	// kq : pub sub
+	UserCommentOptServiceConf kq.KqConf
+
+	// rpc
+	UserCommentRpc zrpc.RpcClientConf
+}
