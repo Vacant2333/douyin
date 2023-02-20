1acabdfd6f60714ad7562cf8ea8dffbb6e02f064
田冰航 <1312186639@qq.com>
2023-02-20 08:56:46 +0800

    Edit publishVideoLogic.go

--- pkg/video/rpc/internal/logic/publishvideologic.go
+++ pkg/video/rpc/internal/logic/publishvideologic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"douyin/common/model/videoModel"
+	"time"
 
 	"douyin/pkg/video/rpc/internal/svc"
 	"douyin/pkg/video/rpc/types/video"
@@ -24,7 +26,27 @@ func NewPublishVideoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Publ
 }
 
 func (l *PublishVideoLogic) PublishVideo(in *video.PublishVideoReq) (*video.PublishVideoResp, error) {
-	// todo: add your logic here and delete this line
+	// todo: 根据token获取authorId
+	var authorId int64 = 1
+	// todo: 调用minIO RPC发送，获得videoURL,frontImgURL
+	videoURL, frontImgURL := "I am videoURL", "I am frontImgURL"
 
-	return &video.PublishVideoResp{}, nil
+	publishVideo := &videoModel.Video{
+		AuthorId: authorId,
+		PlayUrl:  videoURL,
+		CoverUrl: frontImgURL,
+		Time:     time.Now().Unix(),
+		Title:    in.Title,
+	}
+	_, err := l.svcCtx.VideoModel.Insert(l.ctx, publishVideo)
+	if err != nil {
+		return &video.PublishVideoResp{
+			StatusCode: -1,
+			StatusMsg:  "Failed, something seems to be wrong on the server side",
+		}, err
+	}
+	return &video.PublishVideoResp{
+		StatusCode: 0,
+		StatusMsg:  "success",
+	}, nil
 }
