10041accd338b40cb87b230c9c013823debddb2e
田冰航 <1312186639@qq.com>
2023-02-20 10:38:08 +0800

    fix get video logic

--- pkg/video/rpc/internal/logic/getvideologic.go
+++ pkg/video/rpc/internal/logic/getvideologic.go
@@ -31,22 +31,36 @@ func (l *GetVideoLogic) GetVideo(in *video.GetVideoReq) (*video.GetVideoResp, er
 	if err != nil {
 		return nil, err
 	}
-	videos := make([]*video.Video, selectNum)
+	videos := make([]*video.Video, len(queryVideos))
 	var wg sync.WaitGroup
 	for i, queryVideo := range queryVideos {
 		query := queryVideo
 		wg.Add(1)
 		go func(i int, queryVideos []*videoModel.Video) {
-			videos[i].Id = query.Id
-			videos[i].PlayUrl = query.PlayUrl
-			videos[i].CoverUrl = query.CoverUrl
-			videos[i].FavoriteCount = query.LikeCount
-			videos[i].CommentCount = query.CommentCount
-			videos[i].Title = query.Title
-			// todo: 调用RPC获取作者信息
-			videos[i].Author = nil
+			videos[i] = &video.Video{
+				Id:            query.Id,
+				PlayUrl:       query.PlayUrl,
+				CoverUrl:      query.CoverUrl,
+				FavoriteCount: query.LikeCount,
+				CommentCount:  query.CommentCount,
+				Title:         query.Title,
+			}
+			// todo: 调用RPC获取作者信息，以及user是否关注了该人
+			videos[i].Author = &video.User{
+				Id:              "123",
+				Name:            "Author",
+				FollowCount:     12,
+				FollowerCount:   34,
+				IsFollow:        false,
+				Avatar:          "",
+				BackgroundImage: "",
+				Signature:       "this is a sign",
+				TotalFavorited:  16,
+				WorkCount:       0,
+				FavoriteCount:   15,
+			}
 			// todo: 调用RPC获取是否点赞
-			videos[i].IsFavorite = true
+			videos[i].IsFavorite = false
 			defer wg.Done()
 		}(i, queryVideos)
 	}
