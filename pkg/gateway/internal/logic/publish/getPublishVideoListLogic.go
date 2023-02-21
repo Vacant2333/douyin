5d178ef49c1ea04fffc50ed1c291c62ca9db3785
田冰航 <1312186639@qq.com>
2023-02-22 02:02:02 +0800

    Add getPublishedVideoList api to gateway

--- pkg/gateway/internal/logic/publish/getPublishVideoListLogic.go
+++ pkg/gateway/internal/logic/publish/getPublishVideoListLogic.go
@@ -2,8 +2,10 @@ package publish
 
 import (
 	"context"
+	"douyin/common/xerr"
 	"douyin/pkg/gateway/internal/svc"
 	"douyin/pkg/gateway/internal/types"
+	"douyin/pkg/video/rpc/types/video"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -23,7 +25,46 @@ func NewGetPublishVideoListLogic(ctx context.Context, svcCtx *svc.ServiceContext
 }
 
 func (l *GetPublishVideoListLogic) GetPublishVideoList(req *types.GetPubVideoListReq) (resp *types.GetPubVideoListRes, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	videosResp, err := l.svcCtx.VideoRPC.GetAllVideoByUserId(l.ctx, &video.GetAllVideoByUserIdReq{
+		Token:  req.Token,
+		UserId: req.UserId,
+	})
+	if err != nil {
+		logx.Errorf("get videos by userID failed: %v", err.Error())
+		return &types.GetPubVideoListRes{
+			Status: types.Status{
+				Code: xerr.ERR,
+				Msg:  "get videos failed",
+			},
+		}, err
+	}
+	videos := make([]*types.PubVideo, len(videosResp.VideoList))
+	for i, v := range videosResp.VideoList {
+		videos[i].Id = v.Id
+		videos[i].User = types.User{
+			UserId:          v.Author.Id,
+			UserName:        v.Author.Name,
+			FollowCount:     v.Author.FollowCount,
+			FollowerCount:   v.Author.FollowerCount,
+			IsFollow:        v.Author.IsFollow,
+			Avatar:          v.Author.Avatar,
+			BackgroundImage: v.Author.BackgroundImage,
+			Signature:       v.Author.Signature,
+			TotalFavorited:  v.Author.TotalFavorited,
+			WorkCount:       v.Author.WorkCount,
+			FavoriteCount:   v.Author.FavoriteCount,
+		}
+		videos[i].PlayURL = v.PlayUrl
+		videos[i].CoverURL = v.CoverUrl
+		videos[i].FavoriteCount = int(v.FavoriteCount)
+		videos[i].CommentCount = int(v.CommentCount)
+		videos[i].IsFavorite = v.IsFavorite
+		videos[i].Title = v.Title
+	}
+	return &types.GetPubVideoListRes{
+		Status: types.Status{
+			Code: xerr.OK,
+		},
+		VideoPubList: videos,
+	}, nil
 }
