d03b7beea1dffe341c660a4ee3dc21ea4f750898
田冰航 <1312186639@qq.com>
2023-02-23 10:45:11 +0800

    Add ChangeVideoCommentCount RPC to comment

--- pkg/comment/internal/svc/servicecontext.go
+++ pkg/comment/internal/svc/servicecontext.go
@@ -3,17 +3,21 @@ package svc
 import (
 	"douyin/common/model/commentModel"
 	"douyin/pkg/comment/internal/config"
+	"douyin/pkg/video/videoservice"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
 	Config           config.Config
 	UserCommentModel commentModel.CommentModel
+	VideoRPC         videoservice.VideoService
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:           c,
 		UserCommentModel: commentModel.NewCommentModel(sqlx.NewMysql(c.DB.DataSource), c.CacheConf),
+		VideoRPC:         videoservice.NewVideoService(zrpc.MustNewClient(c.VideoRPC)),
 	}
 }
